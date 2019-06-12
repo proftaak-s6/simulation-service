a6df11acb1280995e26f2a0700b1e222e98b1463
reinoudvanzoelen <reinoud95@hotmail.com>
2019-06-12 14:07:37 +0200

    get api key from kumuluz

--- src/main/java/services/GoogleGeocodingService.java
+++ src/main/java/services/GoogleGeocodingService.java
@@ -7,16 +7,16 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
+import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
+
 import models.google.GoogleGeocodedLocation;
 import models.google.Location;
 
 @Dependent
 public class GoogleGeocodingService {
-    // latlng=40.714224,-73.961452&key=YOUR_API_KEY
-
     private String API_URL = "https://maps.googleapis.com/maps/api/geocode/json?";
-    // De eerste die hier de api key plaatst en dat op git zet krijgt slaag van Eva
-    private String API_KEY = "<enter the key here>";
+    private String API_KEY = ConfigurationUtil.getInstance().get("config.google.directionsApi.key")
+            .orElseThrow(() -> new RuntimeException("Could not find 'config.google.directionsApi.key'"));
 
     private Client client;
     private WebTarget target;
@@ -25,15 +25,15 @@ public class GoogleGeocodingService {
         client = ClientBuilder.newClient();
     }
 
-    public GoogleGeocodedLocation getDirections(Location location) {
+    public GoogleGeocodedLocation getLocation(Location location) {
         target = client.target(API_URL).queryParam("latlng", location.getLat() + "," + location.getLng())
                 .queryParam("key", API_KEY);
 
         return target.request(MediaType.APPLICATION_JSON).get(GoogleGeocodedLocation.class);
     }
 
-    public String getDirectionsUrl(String origin, String destination) {
-        target = client.target(API_URL).queryParam("origin", origin).queryParam("destination", destination)
+    public String getLocationUrl(Location location) {
+        target = client.target(API_URL).queryParam("latlng", location.getLat() + "," + location.getLng())
                 .queryParam("key", API_KEY);
 
         return target.getUri().toString();
